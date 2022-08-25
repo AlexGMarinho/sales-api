9deb1135d6bcbf721d7457ce8d2588b620b98259
AlexGMarinho <alexg.marinhoooo>
2022-08-25 11:40:45 -0300

    encrypting password

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -33,5 +33,5 @@ app.use((error: Error, req: Request, res: Response) => {
 });
 
 app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+  console.log('Server startd on http://localhost:3000 🏆');
 });
