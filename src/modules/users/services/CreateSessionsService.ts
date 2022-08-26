1237baa050e6853db37db5d30e6d98b5c52a46a7
AlexGMarinho <alexg.marinhoo@gmail.com>
2022-08-26 16:15:06 -0300

    JWT token

--- src/modules/users/services/CreateSessionsService.ts
+++ src/modules/users/services/CreateSessionsService.ts
@@ -1,5 +1,6 @@
 import AppError from '@shared/errors/AppError';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import { UserRepository } from '../typeorm/repositories/UsersRepository';
@@ -9,20 +10,30 @@ interface IRequest {
   password: string;
 }
 
+interface IResponse {
+  user: User;
+  token: string;
+}
+
 export default class CreateSessionsService {
-  public async execute({ email, password }: IRequest): Promise<User> {
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
     const usersRepository = getCustomRepository(UserRepository);
 
     const user = await usersRepository.findByEmail(email);
     if (!user) {
-      throw new AppError('Incorrect email/password combination', 401);
+      throw new AppError('Incorrect email combination', 401);
     }
 
     const passwordConfirmed = await compare(password, user.password);
     if (!passwordConfirmed) {
-      throw new AppError('Incorrect email/password combination', 401);
+      throw new AppError('Incorrect password combination', 401);
     }
 
-    return user;
+    const token = sign({}, '5d4d0a34d7477b70f314a12853a840e793267a32', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return { user, token };
   }
 }
