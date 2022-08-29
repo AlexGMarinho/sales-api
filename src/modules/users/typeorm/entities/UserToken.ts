a748df779fe39aa6a94744f2efc0ea6d548ecc8f
AlexGMarinho <alexg.marinhoo@gmail.com>
2022-08-29 07:32:30 -0300

    UserToken

--- src/modules/users/typeorm/entities/UserToken.ts
+++ src/modules/users/typeorm/entities/UserToken.ts
@@ -2,26 +2,22 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  Generated,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
 
-@Entity('users')
-export default class User {
+@Entity('user')
+export default class UserToken {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Column()
-  name: string;
+  @Generated('uuid')
+  token: string;
 
   @Column()
-  email: string;
-
-  @Column()
-  password: string;
-
-  @Column()
-  avatar: string;
+  user_id: string;
 
   @CreateDateColumn()
   created_at: Date;
