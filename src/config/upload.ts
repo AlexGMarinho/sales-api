5d9bca8e43f7633e98802f6b7803c5109d2fe58b
AlexGMarinho <alexg.marinhoo@gmail.com>
2022-08-29 06:56:20 -0300

    delete autheticated

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -3,6 +3,7 @@ import path from 'path';
 import crypto from 'crypto';
 
 const uploadFolder = path.resolve(__dirname, '..', '..', 'uploads');
+// Exportando o OBJECT que vai conter a config do multer, que é a biblioteca que vai lha dar com upload
 
 export default {
   director: uploadFolder,
